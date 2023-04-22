c472bfc150bdd40d6cfc1da400ec76dee9686ee5
TingSyuanWang <vincent@fishboneapps.com>
2023-04-22 01:26:34 -0400

    feat: get complete repo data from big five companies except microsoft
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ big_companies/README.md
@@ -0,0 +1,7 @@
+# Big Tech Companies
+## [Big Five](https://en.wikipedia.org/wiki/Big_Tech)
+1. [Google](https://github.com/google)
+2. [Amazon](https://github.com/amzn)
+3. [Apple](https://github.com/apple)
+4. [Meta](https://github.com/facebook) 
+5. [Microsoft](https://github.com/microsoft)
---
+++ big_companies/get_microsoft.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+from dotenv import load_dotenv
+from github import Github
+
+# Load environment variables
+load_dotenv()
+
+# using an access token
+github_access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+g = Github(github_access_token)
+
+org = g.get_organization("microsoft")
+S
+for index, repo in org.get_repos():
+    data = [repo.id, repo.name, repo.full_name, repo.raw_data['owner']['type'], repo.topics, repo.visibility,
+            repo.language,
+            repo.languages_url, repo.created_at, repo.last_modified, repo.stargazers_count, repo.forks,
+            repo.watchers_count, repo.network_count, repo.open_issues_count, repo.url]
+    # Write to CSV
+    with open("collect_data/microsoft.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
